ead92e6739e91c1c37383c3dba8c6446c709eb3b
Benjamin San Souci <benjamin.sansouci@gmail.com>
2013-10-03 22:12:58 -0700

    Nicer implementation
    Added WinIcon file (actually changed the name from MyIcon to WinIcon,
    but I also changed some parts of the code)
    
    Removed Clickable.js -> this is a functional language, why would I
    encapsulate? Actually I made a mistake doing that, when creating a
    Clickable object, then creating an Icon on top of that, the functions
    mouse_down() weren't aware of the existence of the Icon, which is kind
    of the whole point.
    
    Now Windows open files instead of raw HTML strings, which makes
    everything nicer.
    
    Added some comments.

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
-jWinUI --- Version 0.2
+jWinUI --- Version 0.3
 
 This is a simple Windows UI in JavaScript.
 
 ---------------------------------------------
 
-Here is a sample code:
+How do you use it you might ask? Well it's pretty simple:
+
+- Create a window in the body of the page:
+```Javascript
+var width = 640, height = 480;
+var place = document.body;
+var title = "I am a title";
+var url = "aPage.html";
+
+var win = Window(width, height, place, title, url);
+```
+
+Width => width in px of the Window
+Height => height in px of the Window
+place => the dom element into which you want your Window to be rendered
+title => the title that will appear on the Window
+url => the url of the local html file that you want to load inside this new Window
 
 ```Javascript
 $(document).ready(function(){
 	window_init();
-
-	var win = Window(640, 480, document.body, "Title", "HTML content");
-	var icon = WinIcon(50, 50, "Title", "HTML content", "myIconNotClicked.jpg", "myIconClicked.jpg");
+	var win = Window(640, 480, document.body, "TEST", "a_page.html");
+	var icon = WinIcon(50, 50, "Title", "url.html", "myIconNotClicked.jpg", "myIconClicked.jpg");
 });
 ```
--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// API ->
-// Window(width, height, place, text, title, x, y)
-// changeTextWindow(win, text, title)
-// function Icon(url_pic, function_click, classe_hover, x, y)
-// function MyIcon(url_pic, x, y, name, data)
-
-
-
 $(document).ready(function(){
 	window_init();
 
-	var win = Window(640, 480, document.body, "TEST", "Test");
-	var win2 = Window(640, 480, document.body, "TEST2", "Test2", 200, 300);
-	var win2 = Window(640, 480, document.body, "TEST3", "Test3", 200, 100);
+	var win = Window(640, 480, document.body, "TEST", "test.html");
 
-	var icon = WinIcon(50, 50, "LOLs", "LOL", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_btn.jpg", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_disbtn.jpg");
-	var icon = WinIcon(200, 50, "This is a test", "<span style='color:red;'>RED</span>", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_btn.jpg", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_disbtn.jpg");
+	var icon = WinIcon(50, 50, "LOLs", "test.html", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_btn.jpg", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_disbtn.jpg");
+	var icon = WinIcon(200, 50, "This is a test", "test.html", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_btn.jpg", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_disbtn.jpg");
 });
