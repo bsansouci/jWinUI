f317319e3573557ccd06a2b367dabdca87053649
Benjamin San Souci <benjamin.sansouci@gmail.com>
2013-10-04 00:27:54 -0700

    Small changes and behavioral bug fixes
    Added a title for a WinIcon and a smoother hover background, with
    roundish edges.
    Fixed the bug where when you extend a page, it doesn't extend it's
    content.
    Fixed the "focus" problem because it was setting cur = $(this.iDiv)
    inside focus, and it had nothing to do here.
    Created another test page which will be nicely formatted later.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-	var icon = WinIcon(50, 50, "My Computer", "test.html", "./img/folder_icon.png", "./img/folder_icon_clicked.png");
-	var icon = WinIcon(50, 150, "My Documents", "test.html", "./img/folder_icon.png", "./img/folder_icon_clicked.png");
+	var icon = WinIcon(50, 50, "Portfolio", "test.html", "./img/folder_icon.png", "./img/folder_icon_clicked.png");
+	var icon = WinIcon(50, 200, "My Documents", "test2.html", "./img/folder_icon.png", "./img/folder_icon_clicked.png");
 });
