a254e9b826a344ade85db31fe4c3f47ec951cf22
Benjamin San Souci <benjamin.sansouci@gmail.com>
2013-10-03 20:41:21 -0700

    Numerous bug fixes...
    I finally found the problem where windows wouldn't "go in front" when
    moved, it was because I was called
    $(all_windows[i]).css(...)
    instead of
    $(all_windows[i].iDiv).css(...)
    And jQuery didn't complain! "Javascript is loosely typed" they said.

--- README.md
+++ README.md
@@ -10,8 +10,7 @@ Here is a sample code:
 $(document).ready(function(){
 	window_init();
 
-	var win = Window(640, 480, document.body, "TEST", "Test");
+	var win = Window(640, 480, document.body, "Title", "HTML content");
 	var icon = WinIcon(50, 50, "Title", "HTML content", "myIconNotClicked.jpg", "myIconClicked.jpg");
-	
 });
 ```
--- script.js
+++ script.js
@@ -10,6 +10,9 @@ $(document).ready(function(){
 	window_init();
 
 	var win = Window(640, 480, document.body, "TEST", "Test");
+	var win2 = Window(640, 480, document.body, "TEST2", "Test2", 200, 300);
+	var win2 = Window(640, 480, document.body, "TEST3", "Test3", 200, 100);
+
 	var icon = WinIcon(50, 50, "LOLs", "LOL", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_btn.jpg", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_disbtn.jpg");
-	
+	var icon = WinIcon(200, 50, "This is a test", "<span style='color:red;'>RED</span>", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_btn.jpg", "http://www.hiveworkshop.com/forums/resource_images/14/icons_13978_disbtn.jpg");
 });
