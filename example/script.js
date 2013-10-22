3ba5ca1aaf724dc6cab51054efdc7ddfdbf5fd1c
Benjamin San Souci <benjamin.sansouci@gmail.com>
2013-10-22 07:52:53 -0400

    New cssParse + minor changes
    Created a cssParser that will help keeping each window seperate, now
    matter what the code is. It will add the ID of the window in from of
    every DOM element to avoid conflicts with other windows.

--- example/script.js
+++ example/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
 	var portfolioWindow = Window("test.html", "Portfolio", 50, 50);
 	var myDocumentsWindow = Window("test2.html", "My Documents", 50, 200);
-	var icon = WinIcon(portfolioWindow, "./img/folder_icon.png", "./img/folder_icon_clicked.png", 50, 50);
-	var icon2 = WinIcon(myDocumentsWindow, "./img/folder_icon.png", "./img/folder_icon_clicked.png", 150, 50);
+	var icon = WinIcon(portfolioWindow, "img/folder_icon.png", "img/folder_icon_clicked.png", 50, 50);
+	var icon2 = WinIcon(myDocumentsWindow, "img/folder_icon.png", "img/folder_icon_clicked.png", 150, 50);
+
+	parseCSS('style.css', function (array) {
+		rewriteCSSFile('style.css', 10, array);
+	});
 });
