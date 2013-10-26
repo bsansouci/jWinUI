631ce71f887d6139ff04c03ee05ce67df31049f6
Benjamin San Souci <benjamin.sansouci@gmail.com>
2013-10-26 04:37:22 -0400

    Tons of changes
    Added the ability to right click.
    Added the Settings window to try out what you could do with right click.
    Big changes in the code: global object that contains everything (better
    encapsulation, avoids random global variables like ALL_WINDOWS which
    isn't interesting for the user).
    Changing the API's name to Please.
    Bug correction:
    - closing windows that would close other ones (because of bad event
    handling)
    - click on window wouldn't focus
    - right click on button would focus on the window
    
    CSSParser isn't working, nor is HTMLParser (I haven't tested it quite
    yet)

--- example/script.js
+++ example/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-	var portfolioWindow = Window("test.html", "Portfolio", 50, 50);
-	var myDocumentsWindow = Window("test2.html", "My Documents", 50, 200);
-	var icon = WinIcon(portfolioWindow, "img/folder_icon.png", "img/folder_icon_clicked.png", 50, 50);
-	var icon2 = WinIcon(myDocumentsWindow, "img/folder_icon.png", "img/folder_icon_clicked.png", 150, 50);
+	var win1 = Please.createWindow("test.html", "Window1", 200, 50);
+	var win2 = Please.createWindow("test2.html", "Window2", 200, 200);
+	var settings = Please.createWindow("settings.html", "Settings", 800, 200, 300, 300);
 
-	parseCSS('style.css', function (array) {
-		rewriteCSSFile('style.css', 10, array);
-	});
+	var icon = WinIcon(win1, "img/folder_icon.png", "img/folder_icon_clicked.png", 50, 50);
+	var icon2 = WinIcon(win2, "img/folder_icon.png", "img/folder_icon_clicked.png", 150, 50);
 });
